d4aa434af0875a9533b8d4d19a60e707c18c1ca0
IceCrow14 <146310428+IceCrow14@users.noreply.github.com>
2023-11-12 01:34:11 -0600

    Removed a residual file. Finished v0.3 pre-release.

--- build.cmd
+++
@@ -1,4 +0,0 @@
-REM Change directory to root
-REM .\build\srlua\bin\glue.exe .\build\srlua\bin\srlua.exe .\main.lua .\main.exe
-
-.\build\srlua\bin\glue.exe .\build\srlua\bin\srlua.exe ..\srlua_test\test.lua .\testeoso.exe
